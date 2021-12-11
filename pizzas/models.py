8c55d1e06d10ed977d5ab64743728b4f764f8e76
sydneycheng <sydney_cheng1@baylor.edu>
2021-12-11 16:15:34 -0600

    define topping model

--- pizzas/models.py
+++ pizzas/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Pizza(models.Model):
     name = models.CharField(max_length=200)
+    # auto_now_add=True - set this attribute to the current date and time
     date_added = models.DateTimeField(auto_now_add=True)
 
+    # Allows the "name" of the Pizza to be displayed
     def __str__(self):
         return self.name
 
@@ -13,3 +17,11 @@ class Topping(models.Model):
     pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # allows us to set a special attribute telling Django to use "Toppings"
+        # when it needs to refer to more than one topping
+        verbose_name_plural = "Toppings"
+
+    def __str__(self):
+        return f"{self.name[:50]}"
