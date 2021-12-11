e4b14c1fe6edadc6729dee648aad980ab14071f5
sydneycheng <sydney_cheng1@baylor.edu>
2021-12-11 17:04:51 -0600

    create first view and template

---
+++ pizzas/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+# app_name helps django distinguish this urls.py file from files
+# of the same name in other apps within the project
+app_name = "Pizzeria"
+
+
+# is a list of individual pages that can be requested from the
+# Pizzeria app
+urlpatterns = [path("", views.index, name="index")]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# when a URL request matches the pattern we just defined,
+# Django looks for a function called index() in the views.py file.
+
+
+def index(request):
+    """The home page for Pizzeria."""
+    # the View must point to a Template
+    return render(request, "Pizzeria/index.html")
