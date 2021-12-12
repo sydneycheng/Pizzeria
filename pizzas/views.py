a3111708d19a1ec526adc4ce395ab5f390feb0e5
sydneycheng <sydney_cheng1@baylor.edu>
2021-12-12 02:15:32 -0600

    create template for pizzas (pizzas.html)

--- pizzas/views.py
+++ pizzas/views.py
@@ -17,10 +17,11 @@ def index(request):
 # from urls.py
 def pizzas(request):
     pizzas = Pizza.objects.order_by("date_added")
-
     # Key is the name of the variable we'll use in the template (HTML)
+    ## allows us to access the data
     # Value is the variable used in the view function
+    ## this is the data we need to send to the template
     context = {"pizzas": pizzas}
-
     # pass the context dictionary to the template (HTML) -- allows it to be rendered to the browser
+    # the pizzas.html must match the name of our template
     return render(request, "pizzas/pizzas.html", context)
